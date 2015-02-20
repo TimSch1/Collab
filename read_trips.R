b6206ea8279748d40d81f1edbd578b9b67c3a250
TimSch1 <timothy.schmeier@gmail.com>
2015-02-20 08:59:47 -0500

    modified

--- read_trips.R
+++ read_trips.R
@@ -1,4 +1,5 @@
 read_trips = function(x){
+  setwd(x)
   dir_list = list.files(x)
   num_files = length(dir_list)
   files = lapply(dir_list, read.csv)
@@ -9,3 +10,4 @@ read_trips = function(x){
   files = cbind(files, trip, time)
   }
   
+#ggplot(p, aes(x=x,y=y, group=trip, color=trip, label=trip))+geom_path()+geom_text(data= p %>% group_by(trip) %>% filter(y == max(y)), color='black', size=5, position = position_jitter())+scale_color_continuous(low='red', high='orange')
